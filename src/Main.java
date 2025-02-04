92ac9eb1276570f8cd72a1534f34e33b82fc022f
Levin-Alexey <levin.penza@yandex.ru>
2025-02-04 11:03:15 +0300

    the project is ready

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,52 @@
+import functions.TaxCalculation;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting the application...");
+        Scanner scanner = new Scanner(System.in);
+        int earnings = 0;
+        int spendings = 0;
+
+        while (true) {
+
+            System.out.println("Выберите операцию и введите её номер:");
+            System.out.println("1. Добавить новый доход");
+            System.out.println("2. Добавить новый расход");
+            System.out.println("3. Выбрать систему налогообложения");
+
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
+            } else if (input.matches("\\d+")) {
+                int operation = Integer.parseInt(input);
+
+                switch (operation) {
+                    case 1:
+                        System.out.println("Введите сумму дохода: ");
+                        String moneyEarningsStr = scanner.nextLine(); // Не используйте тут nextInt (!)
+                        int moneyEarnings = Integer.parseInt(moneyEarningsStr);
+                        earnings += moneyEarnings;
+                        break;
+                    case 2:
+                        System.out.println("Введите сумму расхода: ");
+                        String moneySpendingsStr = scanner.nextLine(); // Не используйте тут nextInt (!)
+                        int moneySpendings = Integer.parseInt(moneySpendingsStr);
+                        spendings += moneySpendings;
+                        break;
+                    case 3:
+                        int earningsMinusSpendings =  TaxCalculation.getTaxEarningsMinusSpendings(earnings, spendings);
+                        int incomeTax6Percent = TaxCalculation.getIncomeTax6Percent(earnings);
+                        TaxCalculation.showTaxRecommendation(earningsMinusSpendings, incomeTax6Percent);
+                        break;
+                    default:
+                        System.out.println("Такой операции нет");
+                }
+            } else {
+                System.out.println("Введите пожалуста сумму дохода или расхода. Для выхода введите end");
+            }
+        }
+        System.out.println("Программа завершена!");
     }
 }
--- src/functions/TaxCalculation.java
+++ src/functions/TaxCalculation.java
@@ -2,21 +2,28 @@ package functions;
 
 public class TaxCalculation {
 
-    public int getTaxEarningsMinusSpendings(int earnings, int spendings){
+    public static int getTaxEarningsMinusSpendings(int earnings, int spendings){
         int tax = (earnings - spendings) * 15 / 100;
         return Math.max(tax, 0);
     }
 
-    public int getIncomeTax6Percent(int earnings){
-        return earnings * 6 / 100;
+    public static int getIncomeTax6Percent(int earnings){
+        return (earnings * 6) / 100;
     }
 
-    public void showTaxRecommendation(int earningsMinusSpendings, int incomeTax6Percent){
+    public static void showTaxRecommendation(int earningsMinusSpendings, int incomeTax6Percent){
         if (earningsMinusSpendings < incomeTax6Percent){
             System.out.println("Мы советуем Вам - УСН доходы минус расходы");
             System.out.println("Ваш налог составит: " + earningsMinusSpendings + " рублей");
             System.out.println("Налог на другой системе: " + incomeTax6Percent + " рублей");
             System.out.println("Экономия: " + (incomeTax6Percent - earningsMinusSpendings) + " рублей");
+        } else if (earningsMinusSpendings > incomeTax6Percent) {
+            System.out.println("Мы советуем Вам - УСН");
+            System.out.println("Ваш налог составит: " + incomeTax6Percent + " рублей");
+            System.out.println("Налог на другой системе: " + earningsMinusSpendings + " рублей");
+            System.out.println("Экономия: " + (earningsMinusSpendings - incomeTax6Percent) + " рублей");
+        } else {
+            System.out.println("Можете выбрать любую систему налогообложения");
         }
     }
 
